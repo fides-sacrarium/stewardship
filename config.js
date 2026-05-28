e5574bec21b95427d817713440b9f61acb4f841c
fides sacrarium <159918539+fides-sacrarium@users.noreply.github.com>
2026-05-28 14:15:50 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// Firebase Configuration - Diocese of Bismarck Stewardship
+const firebaseConfig = {
+  apiKey: "AIzaSyBdQRDdnb7Iw34_O3WJAt8PwPwRnOGlneA",
+  authDomain: "stewardship-bismarck.firebaseapp.com",
+  databaseURL: "https://stewardship-bismarck-default-rtdb.firebaseio.com",
+  projectId: "stewardship-bismarck",
+  storageBucket: "stewardship-bismarck.firebasestorage.app",
+  messagingSenderId: "951374608580",
+  appId: "1:951374608580:web:1abaf3c6ee7311352ff191"
+};
+
+// Admin password (change this!)
+const ADMIN_PASSWORD = "bismarck2026";
